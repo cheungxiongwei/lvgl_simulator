62a45530746cb2e39800b7265528cda37a8677fa
cheungxiongwei <cheungxiongwei@gmail.com>
2026-03-29 10:27:33 +0800

    Enhance `LVGraphics` module with interactive gizmo support and transformations
    - Added `LVGraphicsInteractGizmos` class for handling resizable and rotatable gizmos.
    - Introduced rotation logic in `LVGraphicsItem`.
    - Enabled mouse interaction for selecting, moving, and resizing items in `LVGraphicsScene` and `LVGraphicsView`.
    - Updated example to demonstrate interactive item manipulation.

--- LVGLSDL3/LVGraphics/LVGraphics.cpp
+++ LVGLSDL3/LVGraphics/LVGraphics.cpp
@@ -33,10 +33,20 @@ struct Transform
 class LVGraphicsItem
 {
 public:
+    void rotate(float deg)
+    {
+        m_transform.deg += deg;
+        while (m_transform.deg >= 360)
+            m_transform.deg -= 360;
+
+        m_transform.apply(m_obj);
+    }
+
+
     LVGraphicsItem() = default;
     virtual ~LVGraphicsItem() = default;
     lv_obj_t* to_object() { return m_obj; }
-    lv_obj_t* m_obj;
+    lv_obj_t* m_obj = nullptr;
     Transform m_transform;
 };
 
@@ -59,32 +69,102 @@ public:
 };
 
 
-class LVGraphicsInteractItem
+class LVGraphicsInteractGizmos
 {
 public:
-    LVGraphicsInteractItem()
+    LVGraphicsInteractGizmos(lv_obj_t* parent)
     {
-        m_box = lv_obj_create(lv_scr_act());
+        m_box = lv_obj_create(parent);
         lv_obj_set_scroll_dir(m_box, LV_DIR_NONE);
+        lv_obj_set_flag(m_box, LV_OBJ_FLAG_EVENT_BUBBLE, true);
         lv_obj_set_size(m_box, 1, 1);
+        lv_obj_set_style_bg_opa(m_box, LV_OPA_TRANSP, 0);
         lv_obj_set_style_pad_all(m_box, 0, 0);
         lv_obj_set_style_margin_all(m_box, 0, 0);
 
         lv_obj_set_style_border_color(m_box, lv_color_make(255, 0, 0), 0);
-        lv_obj_set_style_border_width(m_box, 1, 0);
+        lv_obj_set_style_border_width(m_box, 6, 0);
+
+        create_handle(m_tl, LV_ALIGN_TOP_LEFT);
+        create_handle(m_tr, LV_ALIGN_TOP_RIGHT);
+        create_handle(m_bl, LV_ALIGN_BOTTOM_LEFT);
+        create_handle(m_br, LV_ALIGN_BOTTOM_RIGHT);
+        create_handle(m_rot, LV_ALIGN_TOP_MID);
     }
+
+    void create_handle(lv_obj_t* handle, lv_align_t align)
+    {
+        handle = lv_button_create(m_box);
+        lv_obj_set_size(handle, 32, 32);
+        lv_obj_align(handle, align, 0, 0);
+    }
+
+    void set_visible(bool visible) { lv_obj_set_flag(m_box, LV_OBJ_FLAG_HIDDEN, !visible); }
+
     void selected(LVGraphicsItem* item)
     {
         m_item = item;
+        if (m_item == nullptr)
+        {
+            set_visible(false);
+            return;
+        }
+        set_visible(true);
 
         int x = lv_obj_get_x(item->to_object());
         int y = lv_obj_get_y(item->to_object());
+        int w = lv_obj_get_width(item->to_object());
+        int h = lv_obj_get_height(item->to_object());
+
         lv_obj_set_pos(m_box, x, y);
-        lv_obj_set_size(m_box, lv_obj_get_width(item->to_object()), lv_obj_get_height(item->to_object()));
+        lv_obj_set_size(m_box, w, h);
+        lv_obj_update_layout(m_box);
+
+        m_item->m_transform.apply(m_box);
     }
+
+    bool has_selected() { return m_item != nullptr; }
+
+    void mouse_press(lv_point_t point)
+    {
+        if (m_item == nullptr)
+            return;
+
+        int x = lv_obj_get_x(m_box);
+        int y = lv_obj_get_y(m_box);
+
+        m_delta_point.x = point.x - x;
+        m_delta_point.y = point.y - y;
+
+        update_pos(point);
+    }
+
+    void mouse_move(lv_point_t point) { update_pos(point); }
+
+    void mouse_release(lv_point_t point) { update_pos(point); }
+
+    void update_pos(lv_point_t point)
+    {
+        if (m_item == nullptr)
+            return;
+
+        lv_obj_set_pos(m_box, point.x - m_delta_point.x, point.y - m_delta_point.y);
+        lv_obj_update_layout(m_box);
+        lv_obj_set_pos(m_item->to_object(), lv_obj_get_x(m_box), lv_obj_get_y(m_box));
+        lv_obj_set_size(m_item->to_object(), lv_obj_get_width(m_box), lv_obj_get_height(m_box));
+    }
+
+    lv_obj_t* to_object() { return m_box; }
 
     lv_obj_t* m_box;
     LVGraphicsItem* m_item = nullptr;
+    lv_point_t m_delta_point;
+
+    lv_obj_t* m_tl;
+    lv_obj_t* m_tr;
+    lv_obj_t* m_bl;
+    lv_obj_t* m_br;
+    lv_obj_t* m_rot;
 };
 
 class LVGraphicsScene
@@ -106,15 +186,28 @@ public:
     {
         lv_obj_t* active = lv_indev_get_active_obj();
         auto [ok, item] = contains(active);
-        if (ok)
+
+        if (active != m_interact_item->to_object())
+            m_interact_item->selected(ok ? item : nullptr);
+        m_interact_item->mouse_press(point);
+    }
+
+    void mouse_move(lv_event_t* e, lv_point_t point)
+    {
+        std::println("mouse_move: {} {} {}", point.x, point.y, m_interact_item->has_selected());
+        if (m_interact_item->has_selected())
         {
-            m_interact_item.selected(item);
+            m_interact_item->mouse_move(point);
         }
     }
 
-    void mouse_move(lv_event_t* e, lv_point_t point) {}
-
-    void mouse_release(lv_event_t* e, lv_point_t point) {}
+    void mouse_release(lv_event_t* e, lv_point_t point)
+    {
+        if (m_interact_item->has_selected())
+        {
+            m_interact_item->mouse_release(point);
+        }
+    }
 
     void add_item(LVGraphicsItem* item) { m_items.emplace_back(item); }
 
@@ -130,14 +223,16 @@ public:
         return {false, nullptr};
     }
 
+    void set_interact_item(LVGraphicsInteractGizmos* interact_item) { m_interact_item = interact_item; }
+
+
     std::vector<LVGraphicsItem*> m_items;
 
     lv_obj_t* m_obj;
 
     Transform m_transform;
 
-    LVGraphicsInteractItem m_interact_item;
-    lv_point_t m_last_point;
+    LVGraphicsInteractGizmos* m_interact_item;
 };
 
 class LVGraphicsView
@@ -187,21 +282,20 @@ public:
 
     void mouse_press(lv_event_t* e, lv_point_t point)
     {
-        std::println("mouse_press: {} {}", point.x, point.y);
+
         if (m_scene)
             m_scene->mouse_press(e, point);
     }
 
     void mouse_move(lv_event_t* e, lv_point_t point)
     {
-        std::println("mouse_move: {} {}", point.x, point.y);
+
         if (m_scene)
             m_scene->mouse_move(e, point);
     }
 
     void mouse_release(lv_event_t* e, lv_point_t point)
     {
-        std::println("mouse_release: {} {}", point.x, point.y);
         if (m_scene)
             m_scene->mouse_release(e, point);
     }
@@ -222,6 +316,15 @@ void CreateLVGraphics()
     auto scene = new LVGraphicsScene(view->to_object());
     view->set_scene(scene);
 
+    LVGraphicsInteractGizmos* interact_item = new LVGraphicsInteractGizmos(view->to_object());
+    scene->set_interact_item(interact_item);
+
+    auto item1 = new LVGraphicsRectItem(150, 200, 200, 300, scene->to_object());
+    scene->add_item(item1);
     scene->add_item(new LVGraphicsRectItem(50, 50, 100, 200, scene->to_object()));
     scene->add_item(new LVGraphicsRectItem(200, 50, 100, 50, scene->to_object()));
+    scene->add_item(new LVGraphicsRectItem(500, 400, 100, 50, scene->to_object()));
+
+    lv_obj_update_layout(lv_screen_active());
+    item1->rotate(45);
 }
